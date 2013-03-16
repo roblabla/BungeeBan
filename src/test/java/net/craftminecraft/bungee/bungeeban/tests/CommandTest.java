7d52e7361e04d5a4aa080d9d36744bbb0c484d75
Robin Lambertz <robinlambertz.dev@gmail.com>
2013-03-16 22:18:56 +0000

    Finished up one test. Need to write a few hundreds more.

--- src/test/java/net/craftminecraft/bungee/bungeeban/tests/CommandTest.java
+++ src/test/java/net/craftminecraft/bungee/bungeeban/tests/CommandTest.java
@@ -1,6 +1,8 @@
 package net.craftminecraft.bungee.bungeeban.tests;
 
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -18,8 +20,11 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import net.craftminecraft.bungee.bungeeban.BanManager;
+import net.craftminecraft.bungee.bungeeban.banstore.BanEntry;
 import net.craftminecraft.bungee.bungeeban.command.BanCommand;
 import net.md_5.bungee.api.Callback;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -28,34 +33,43 @@ import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Command;
 
 @RunWith(PowerMockRunner.class)
+@PrepareForTest(BanManager.class) // BanManager.class contains static methods
 public class CommandTest {
 
+	@SuppressWarnings("serial")
 	@Test
 	public void testBanCommand() {
+		PowerMockito.mockStatic(BanManager.class);
+		when(BanManager.ban(any(BanEntry.class))).thenReturn(true);
+		
 		ProxiedPlayer mockedBanned = mock(ProxiedPlayer.class);
-		Server mockedServer = mock(Server.class);
-		ProxyServer mockedProxy = mock(ProxyServer.class);
+		when(mockedBanned.getName()).thenReturn("ImATroll");
+
+		ProxiedPlayer mockedSender = mock(ProxiedPlayer.class);
+		when(mockedSender.getName()).thenReturn("ImAnAdmin");
+		when(mockedSender.hasPermission(anyString())).thenReturn(true);
+		
 		Collection<ProxiedPlayer> playerlist = new HashSet<ProxiedPlayer>();
 		playerlist.add(mockedBanned);
 		
+		ServerInfo info = mock(ServerInfo.class);
+		when(info.getName()).thenReturn("testserver");
+		when(info.getPlayers()).thenReturn(playerlist);
+
+		ProxyServer mockedProxy = mock(ProxyServer.class);
+		when(mockedProxy.getServerInfo(anyString())).thenReturn(info);
 		when(mockedProxy.getPlayers()).thenReturn(playerlist);
-		ServerInfo info = new ServerInfo("testserver", new InetSocketAddress(25577)) {
-			
-			@Override
-			public void sendData(String channel, byte[] data) {
-				return;
-			}
-			@Override
-			public void ping(Callback<ServerPing> callback) {
-				return;
-			}
-		};
-		when(mockedBanned.getName()).thenReturn("ImATroll");
-		when(mockedBanned.getServer()).thenReturn(mockedServer);
-		when(mockedServer.getInfo()).thenReturn(info);
-		
+		ProxyServer.setInstance(mockedProxy);
+
 		Command bancomm = new BanCommand();
-		
+		String[] args = new String[5];
+		args[0] = "imatroll";
+		args[1] = "testserver";
+		args[2] = "You";
+		args[3] = "annoy";
+		args[4] = "me";
+		bancomm.execute(mockedSender, args);
+		verify(mockedSender).sendMessage(eq(ChatColor.RED + "imatroll has been banned."));
 	}
 	
 }
