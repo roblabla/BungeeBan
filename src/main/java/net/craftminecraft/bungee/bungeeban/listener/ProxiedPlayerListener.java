fa260f252b890eea9435ea1d38e176b4e795cc53
Robin Lambertz <robinpub@googlemail.com>
2013-04-29 12:08:24 +0100

    Use LoginEvent with intents. \nShould be a big performance improvement. All hail AsyncEvent.

--- src/main/java/net/craftminecraft/bungee/bungeeban/listener/ProxiedPlayerListener.java
+++ src/main/java/net/craftminecraft/bungee/bungeeban/listener/ProxiedPlayerListener.java
@@ -7,28 +7,36 @@ import net.craftminecraft.bungee.bungeeban.BungeeBan;
 import net.craftminecraft.bungee.bungeeban.banstore.BanEntry;
 import net.craftminecraft.bungee.bungeeban.util.Utils;
 import net.md_5.bungee.api.connection.Server;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 
 public class ProxiedPlayerListener implements Listener {
-	BungeeBan plugin;
+	private BungeeBan plugin;
 	public ProxiedPlayerListener(BungeeBan plugin) {
 		this.plugin = plugin;
 	}
 	
 	@Subscribe
-	public void onPlayerJoin(PostLoginEvent e) {
-		BanEntry ban = BanManager.getBan(e.getPlayer().getName(), "(GLOBAL)");
-		if (ban != null) {
-			e.getPlayer().disconnect(Utils.formatMessage(ban.getReason(), ban));
-			return;
-		}
-		ban = BanManager.getBan(e.getPlayer().getAddress().getAddress().getHostAddress(), "(GLOBAL)");
-		if (ban != null) {
-            e.getPlayer().disconnect(Utils.formatMessage(ban.getReason(), ban));
-            return;
-		}
+	public void onPlayerJoin(final LoginEvent e) {
+		e.registerIntent(plugin);
+		plugin.getProxy().getScheduler().runAsync(plugin, new Runnable() {
+			@Override
+			public void run() {
+				BanEntry ban = BanManager.getBan(e.getConnection().getName(), "(GLOBAL)");
+				if (ban != null) {
+					e.completeIntent(plugin);
+					e.setCancelled(true);
+					e.setCancelReason(Utils.formatMessage(ban.getReason(), ban));
+				}
+				ban = BanManager.getBan(e.getConnection().getAddress().getAddress().getHostAddress(), "(GLOBAL)");
+				if (ban != null) {
+		            e.setCancelled(true);
+		            e.setCancelReason(Utils.formatMessage(ban.getReason(), ban));
+				}
+				e.completeIntent(plugin);
+			}
+		});
 	}
 	
 	@Subscribe
